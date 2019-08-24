eb0bfb3ffc6e6715a3ab75498cbcdaee5a69d23e
hasitha <hasithadkr7@gmail.com>
2019-08-24 10:20:33 +0530

    individual mike rain user input

--- code/gen_mike_input.py
+++ code/gen_mike_input.py
@@ -691,36 +691,22 @@ def get_individual_rain(db_adapter, available_station_list, ts_start, ts_end):
         print('merged_df : ', merged_df)
 
 
-def create_hybrid_mike_input(dir_path, run_date, forward, backward):
+def create_hybrid_mike_input(output_path, run_date, forward, backward):
+    print('create_hybrid_mike_input|output_path: ', output_path)
     try:
-        curw_sim_adapter = None
         run_datetime = datetime.strptime('%s %s' % (run_date, '00:00:00'), '%Y-%m-%d %H:%M:%S')
         ts_end = run_datetime + timedelta(days=forward)
         ts_start = run_datetime - timedelta(days=backward)
-        config_path = os.path.join(os.getcwd(), 'config.json')
-        with open(config_path) as json_file:
-            config = json.load(json_file)
-            if 'curw_sim_db_config' in config:
-                curw_sim_db_config = config['curw_sim_db_config']
-            else:
-                exit(2)
-            db_adapter = CurwSimAdapter(curw_sim_db_config['user'], curw_sim_db_config['password'],
-                                              curw_sim_db_config['host'], curw_sim_db_config['db'])
-            print('[ts_end, ts_start] : ', [ts_end, ts_start])
-            available_station_list = db_adapter.get_available_stations(ts_start)
-            if len(available_station_list) > 0:
-                print('available_station_list : ', available_station_list)
-                get_individual_rain(db_adapter, available_station_list, ts_start, ts_end)
-            else:
-                print('no available stations.')
+        print('[ts_end, ts_start] : ', [ts_end, ts_start])
+        available_station_list = db_adapter.get_available_stations(ts_start)
+        if len(available_station_list) > 0:
+            print('available_station_list : ', available_station_list)
+            get_individual_rain(db_adapter, available_station_list, ts_start, ts_end)
+        else:
+            print('no available stations.')
     except Exception as e:
         print('Mike generation error|Exception:', str(e))
         traceback.print_exc()
-        try:
-            if curw_sim_adapter is not None:
-                curw_sim_adapter.close()
-        except Exception as ex:
-            print(str(ex))
 
 
 if __name__ == "__main__":
@@ -746,7 +732,23 @@ if __name__ == "__main__":
             forward = arg
         elif opt in ("-b", "--backward"):
             backward = arg
-    output_path = os.path.join(dir_path, run_date, run_time)
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
-        create_hybrid_mike_input(output_path, run_date, run_time, forward, backward)
+    config_path = os.path.join(os.getcwd(), 'config.json')
+    with open(config_path) as json_file:
+        config = json.load(json_file)
+        dir_path = config['dir_path']
+        if 'curw_sim_db_config' in config:
+            curw_sim_db_config = config['curw_sim_db_config']
+        else:
+            exit(2)
+        db_adapter = CurwSimAdapter(curw_sim_db_config['user'], curw_sim_db_config['password'],
+                                    curw_sim_db_config['host'], curw_sim_db_config['db'])
+        output_path = os.path.join(dir_path, run_date, run_time)
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+            create_hybrid_mike_input(output_path, run_date, run_time, forward, backward)
+        try:
+            if db_adapter is not None:
+                db_adapter.close()
+        except Exception as ex:
+            print(str(ex))
+
